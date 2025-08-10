97726d83b4864bf2b1fdb1fbe5b2700ce2236610
Pavel Lopatin <pashapitch@icloud.com>
2025-08-10 16:04:43 +0500

    First commit,  initialization.

---
+++ src/main/java/com/pizzaapp/spring_artemPizza/SpringArtemPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.pizzaapp.spring_artemPizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArtemPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArtemPizzaApplication.class, args);
+	}
+
+}
